46614b11c6d712d456cd319f083b327139626f52
Billyyyyy3320 <newsbielt703@gmail.com>
2019-12-01 16:00:23 +0800

    feat: style SimpleNewsletter

--- src/SimpleNewsletter.vue
+++ src/SimpleNewsletter.vue
@@ -7,14 +7,16 @@
       type="email"
       name="email"
       aria-label="Email"
-      placeholder="type your email"
+      placeholder="Email"
       v-model="mail"
       required
       autocapitalize="off"
       autocorrect="off"
       data-cy="email"
     />
-    <input type="submit" class="newsletter__button" data-cy="submit" />
+    <button type="submit" class="newsletter__button" data-cy="submit">
+      subscribe
+    </button>
   </form>
 </template>
 
@@ -40,4 +42,40 @@ export default {
 };
 </script>
 
-<style lang="stylus"></style>
+<style lang="stylus">
+.newsletter
+  text-align center
+  width 100%
+  max-width 420px
+  margin 1.5rem auto
+  padding 1.8rem 2.3rem
+  border-radius 3px
+  background #f8f8f8
+  font-size 1rem
+  box-sizing border-box
+  color $textColor
+  &__title
+    font-size 1.7rem
+  &__content
+    margin-top 1.5rem
+    margin-bottom 1.5rem
+    line-height 1.7rem
+  &__input
+    font-size inherit
+    border 1px solid $borderColor;
+    width 100%
+    padding .6rem 1.2rem
+    box-sizing border-box
+    border-radius 3px
+    margin-bottom .8rem
+  &__button
+    font-size inherit
+    border none
+    cursor pointer
+    background $accentColor
+    color #fff
+    padding .6rem 1.8rem
+    box-sizing border-box
+    border-radius 3px
+    width 100%
+</style>
