8597b1528f2e062e72eabf0e949a18bcf4c14ba0
Billyyyyy3320 <newsbielt703@gmail.com>
2019-12-14 16:07:46 +0800

    fix: wrong var in subscribe function

--- src/mailchimpSubscribe.js
+++ src/mailchimpSubscribe.js
@@ -5,7 +5,7 @@ let { endpoint } = JSON.parse(MAILCHIMP_OPTIONS);
 const subscribeToMailchimp = function addToMailchimp(email, fields) {
   const emailEncoded = encodeURIComponent(email);
   endpoint = endpoint.replace(/\/post/g, "/post-json");
-  const listFields = listFields ? "&" + queryString.stringify(fields) : "";
+  const listFields = fields ? "&" + queryString.stringify(fields) : "";
   const queryParams = `&EMAIL=${emailEncoded}${listFields}`;
   const url = `${endpoint}${queryParams}`;
 
