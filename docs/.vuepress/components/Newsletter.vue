2bf963eccf621db83223f5119cb1cb681f8cbddf
Billyyyyy3320 <newsbielt703@gmail.com>
2020-01-02 20:31:03 +0800

    docs: minor tweaks

---
+++ docs/.vuepress/components/Newsletter.vue
@@ -0,0 +1,32 @@
+<template>
+  <SimpleNewsletter v-slot="{ slotProps }">
+    <div class="my-title">{{ slotProps.title }}</div>
+    <div class="my-content">{{ slotProps.content }}</div>
+    <input
+      class="my-input"
+      type="email"
+      name="email"
+      aria-label="Email"
+      placeholder="Email"
+      v-model="slotProps.mail"
+      required
+      autocapitalize="off"
+      autocorrect="off"
+      data-cy="email"
+    />
+    <button type="submit" class="my-button" data-cy="submit">
+      123
+    </button>
+  </SimpleNewsletter>
+</template>
+
+<script>
+import SimpleNewsletter from '../../../src/components/SimpleNewsletter';
+export default {
+  components: { SimpleNewsletter },
+};
+</script>
+
+<style>
+/* your style */
+</style>
